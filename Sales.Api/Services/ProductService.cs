807fcc16a355adf974d02d4614551a92e49c0e8f
Clebão 🥲 <cleber.abreu@globalsys.com.br>
2023-07-11 00:04:55 -0300

    add product service

---
+++ Sales.Api/Services/ProductService.cs
@@ -0,0 +1,40 @@
+using Sales.Api.Models;
+using Sales.Api.Repositories;
+
+namespace Sales.Api.Services;
+
+public class ProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(
+        IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        return await _productRepository.GetProductsAsync();
+    }
+
+    public async Task<Product> GetProductAsync(int id)
+    {
+        return await _productRepository.GetProductAsync(id);
+    }
+
+    public async Task<Product> CreateProductAsync(Product product)
+    {
+        return await _productRepository.CreateProductAsync(product);
+    }
+
+    public async Task UpdateProductAsync(Product product)
+    {
+        await _productRepository.UpdateProductAsync(product);
+    }
+
+    public async Task DeleteProductAsync(int id)
+    {
+        await _productRepository.DeleteProductAsync(id);
+    }
+}
