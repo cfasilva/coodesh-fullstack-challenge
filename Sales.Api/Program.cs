904fefd7c9044bb2c6e67b1a6bb6cbf75e7310e5
Clebão 🥲 <cleber.abreu@globalsys.com.br>
2023-07-11 00:06:01 -0300

    add services to the container

--- Sales.Api/Program.cs
+++ Sales.Api/Program.cs
@@ -1,21 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Sales.Api.Contexts;
+using Sales.Api.Repositories;
+using Sales.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddScoped<ProductService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
