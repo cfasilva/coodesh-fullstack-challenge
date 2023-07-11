9321187ac1feeb0d778659c46a535965e2af13d7
Clebão 🥲 <cleber.abreu@globalsys.com.br>
2023-07-11 00:04:44 -0300

    add product repositories

---
+++ Sales.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using Sales.Api.Models;
+
+namespace Sales.Api.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<Product> GetProductAsync(int id);
+    Task<Product> CreateProductAsync(Product product);
+    Task UpdateProductAsync(Product product);
+    Task DeleteProductAsync(int id);
+}
