e2f36bf81033433dc32baba149b1e6e847d75a81
Clebão 🥲 <cleber.abreu@globalsys.com.br>
2023-07-11 00:19:22 -0300

    add seller service

---
+++ Sales.Api/Services/SellerService.cs
@@ -0,0 +1,40 @@
+using Sales.Api.Models;
+using Sales.Api.Repositories;
+
+namespace Sales.Api.Services;
+
+public class SellerService
+{
+    private readonly ISellerRepository _sellerRepository;
+
+    public SellerService(
+        ISellerRepository sellerRepository)
+    {
+        _sellerRepository = sellerRepository;
+    }
+
+    public async Task<IEnumerable<Seller>> GetSellersAsync()
+    {
+        return await _sellerRepository.GetSellersAsync();
+    }
+
+    public async Task<Seller> GetSellerAsync(int id)
+    {
+        return await _sellerRepository.GetSellerAsync(id);
+    }
+
+    public async Task CreateSellerAsync(Seller seller)
+    {
+        await _sellerRepository.CreateSellerAsync(seller);
+    }
+
+    public async Task UpdateSellerAsync(Seller seller)
+    {
+        await _sellerRepository.UpdateSellerAsync(seller);
+    }
+
+    public async Task DeleteSellerAsync(int id)
+    {
+        await _sellerRepository.DeleteSellerAsync(id);
+    }
+}
